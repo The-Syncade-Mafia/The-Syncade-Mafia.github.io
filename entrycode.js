a053ee10fd1ddb3fa44793e9c3aee76dbe459034
shoesareme <77761814+shoesareme@users.noreply.github.com>
2022-09-22 17:30:22 -0400

    Update entrycode.js

--- entrycode.js
+++ entrycode.js
@@ -1,6 +1,5 @@
-const ec = document.getElementsByName("ec"); 
-
 function sumbit() {
+  const ec = document.getElementsByName("ec"); 
   if ec == "XXJUYCBFU3GBCN3U9" {
     document.location = "/dweb/darkweb.md";
   } 
